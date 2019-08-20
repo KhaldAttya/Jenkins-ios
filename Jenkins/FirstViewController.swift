0cc96fb7daec52e01cd2739c151c84de49665e8a
Khaled Attia <khaled.attia@vodafone.com>
2019-08-20 14:02:58 +0200

    initial commit

---
+++ Jenkins/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Jenkins
+//
+//  Created by My Vodafone Core Team on 8/20/19.
+//  Copyright © 2019 My Vodafone Core Team. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JenkinsTests/JenkinsTests.swift
@@ -0,0 +1,34 @@
+//
+//  JenkinsTests.swift
+//  JenkinsTests
+//
+//  Created by My Vodafone Core Team on 8/20/19.
+//  Copyright © 2019 My Vodafone Core Team. All rights reserved.
+//
+
+import XCTest
+@testable import Jenkins
+
+class JenkinsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
