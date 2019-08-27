da4646d0b53e09dd1cd0f714c234af156106dceb
Khaled Attia <khaled.attia@vodafone.com>
2019-08-27 16:32:33 +0200

    Added ipa

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd /Users/myvfcore/Documents/GitHub/Jenkins-ios/
+xcodebuild -project Jenkins.xcodeproj -scheme Jenkins -derivedDataPath /Users/myvfcore/Documents/GitHub/build -allowProvisioningUpdates
+cd /Users/myvfcore/Documents/GitHub/build/Build/products/Debug-iphoneos/
+mkdir Payload
+mv Jenkins.app Payload/Jenkins.app
+zip -r Jenkins.ipa Payload/
+mv Jenkins.ipa /Users/myvfcore/Documents/GitHub/Jenkins-ios/
