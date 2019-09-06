716bd5dfe231fac2f01c6d4cf678f29e1f75c68a
Khaled Attia <khaled.attia@vodafone.com>
2019-09-06 05:12:18 +0200

    hello aws

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
+cd /Users/myvfcore/Documents/GitHub/
+rm -rf build
 cd /Users/myvfcore/Documents/GitHub/Jenkins-ios/
-xcodebuild -project Jenkins.xcodeproj -scheme Jenkins -derivedDataPath /Users/myvfcore/Documents/GitHub/build -allowProvisioningUpdates
+xcodebuild -project Jenkins.xcodeproj -scheme Jenkins -derivedDataPath /Users/myvfcore/Documents/GitHub/build -allowProvisioningUpdates 
 cd /Users/myvfcore/Documents/GitHub/build/Build/products/Debug-iphoneos/
 mkdir Payload
 mv Jenkins.app Payload/Jenkins.app
